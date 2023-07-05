c5a162f82496b1f9843919a99c40d6eb6b1fa0da
Gong-air <77915156+Gong-air@users.noreply.github.com>
2023-07-05 13:29:33 +0800

    Update export_runtime.py

--- docs/_dummy/export_runtime.py
+++ docs/_dummy/export_runtime.py
@@ -1,2 +1,24 @@
-diopiTensor , diopiSize, diopiScalar, diopiReduction, diopiRoundMode, diopiError, TensorP, Context, Device, Dtype, \
-    diopi_tensor_copy_to_buffer, get_last_error_string, finalize_library, diopi_finalize = 1,2,3,4,5,6,7,8,9,10,11,12,13,14
+# diopiTensor , diopiSize, diopiScalar, diopiReduction, diopiRoundMode, diopiError, TensorP, Context, Device, Dtype, \
+#     diopi_tensor_copy_to_buffer, get_last_error_string, finalize_library, diopi_finalize = 1,2,3,4,5,6,7,8,9,10,11,12,13,14
+
+
+
+
+diopiTensor = object
+diopiSize = object
+diopiScalar = object
+diopiReduction = object
+diopiRoundMode = object
+diopiError = object
+TensorP = object
+Context = object
+Device = object
+Dtype = object
+def diopi_tensor_copy_to_buffer(context, tensor, ptr):
+    return 
+def diopiGetLastErrorString():
+    return
+def finalize_library():
+    return
+def diopi_finalize():
+    return
