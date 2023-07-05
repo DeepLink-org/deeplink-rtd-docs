a6c9931864001bcb75b93e4d0b90f6ebba796790
gqw <994321849@qq.com>
2023-07-05 11:28:00 +0800

    test import error

--- docs/gqw/test_import.py
+++ docs/gqw/test_import.py
@@ -4,3 +4,5 @@ import numpy as np
 import atexit
 from export_runtime import diopiTensor, diopiSize, diopiScalar, diopiReduction, diopiRoundMode, diopiError, TensorP, Context, Device, Dtype, \
     diopi_tensor_copy_to_buffer, get_last_error_string, finalize_library, diopi_finalize
+
+print("okayokayokay")
